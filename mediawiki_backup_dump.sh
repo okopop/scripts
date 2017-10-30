5802be639c1e4f9f7bcd1ab97f48e19f6b5da355
OKO <ukusic@gmail.com>
2017-10-30 18:35:55 +0100

    minor fixes

--- mediawiki_backup_dump.sh
+++ mediawiki_backup_dump.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
-# mediawiki content xml dump. monthly rotation 
-# tested on centos/rhel
+# mediawiki content xml dump. monthly rotation
+# tested on rhel
 
 PID_CHECK="/usr/sbin/pidof"
-PID_NAME="mediawikipidname"
-HTTP_URL="https://www.google.se/"
+PID_NAME="/usr/libexec/mysqld"
+HTTP_URL="localhost:443"
 HTTP_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" $HTTP_URL)
-BACKUP_FOLDER="/tmp/mw/backup/content-dump/"
-PHP_CMD="/tmp/mw/php"
-
+BACKUP_FOLDER="/x/"
+PHP_CMD="/bin/php"
+ 
 DUMP_DATE=$(date +"%d")
-DUMP_CMD="/tmp/mw/dumpBackup.php"
-DUMP_PARAMETERS="--full"
+DUMP_CMD="/var/www/mediawiki/maintenance/dumpBackup.php"
+DUMP_PARAMETERS="--quiet --full"
 DUMP_FILENAME="mediawiki-full-dump"
-
+ 
 err () {
-    /usr/bin/logger "[ERROR Mediawiki backup]" $@
+    /bin/logger -t "[ERROR] Mediawiki backup" $@
     exit 1
 }
-
-# pre-checks before backup 
+ 
+# pre-checks before backup
 if [ "$HTTP_STATUS" -ne "200" ]; then err "Http address not reachable. Content backup aborted"; fi
-$PID_CHECK $PID_NAME >/dev/null || err "Mediawiki process not started. Content backup aborted"
-[ -d $BACKUP_FOLDER ] || err "Backup folder is missing. Content backup aborted" 
+$PID_CHECK $PID_NAME >/dev/null || err "MariaDB process not started. Content backup aborted"
+[ -d $BACKUP_FOLDER ] || err "Backup folder is missing. Content backup aborted"
 [ -f $PHP_CMD ] || err "PHP is missing. Content backup aborted"
-[ -f $DUMP_CMD ] || err "Dump script is missing. Content backup aborted" 
-
-# execute mediawiki xml dump
-# php dumpBackup.php --full > dump.xml
+[ -f $DUMP_CMD ] || err "Dump script is missing. Content backup aborted"
+ 
+# execute mediawiki xml dump (php dumpBackup.php --full > dump.xml)
 $PHP_CMD $DUMP_CMD $DUMP_PARAMETERS > $BACKUP_FOLDER$DUMP_FILENAME-$DUMP_DATE.xml
-
+/bin/gzip -f $BACKUP_FOLDER$DUMP_FILENAME-$DUMP_DATE.xml
+ 
 if [ $? -eq "0" ]; then
-    /usr/bin/logger "[INFO Mediawiki backup] Content xml dump was successful: $BACKUP_FOLDER$DUMP_FILENAME-$DUMP_DATE.xml"
+    /bin/logger -t "[INFO] Mediawiki backup" "Content xml dump was successful: $BACKUP_FOLDER$DUMP_FILENAME-$DUMP_DATE.xml"
 else
     err "Execution of dump command failed"
 fi
