f0d1c7bbf25b0c48e33a63c0cba8863cfc578ee0
OKO <ukusic@gmail.com>
2020-04-16 22:24:39 +0200

    styleguide fixes

--- chc-send.sh
+++ chc-send.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
+set -eou pipefail
 CHC_COUNT=$1
-CHC_TO_SEND=`expr $CHC_COUNT - 1000`
+CHC_TO_SEND=$(( $CHC_COUNT - 1000 ))
 echo "$CHC_COUNT is current count"
-echo "$CHC_TO_SEND chc to send"
 
 if [ $CHC_TO_SEND -ge "1" ]; then
-    echo "send_cmd chc=$CHC_TO_SEND"
-    echo "Write this to LOGFILE -> $(date +"[%F %T]") send_cmd chc=$CHC_TO_SEND"
-    
+  echo "send_cmd chc=$CHC_TO_SEND"
+  echo "Write this to LOGFILE -> $(date +"[%F %T]") send_cmd chc=$CHC_TO_SEND"
 else
-    echo "No leftover chc to send"
-    exit 1
+  echo "No leftover chc to send"
+  exit 1
 fi
