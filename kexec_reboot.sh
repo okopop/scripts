11e7e59a731e5c291a04e100c93c94cbb793691e
OKO <ukusic@gmail.com>
2020-04-25 00:40:12 +0200

    first

---
+++ kexec_reboot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# reboot faster after kernel upgrade
+# dep: kexec-tools
+set -eu
+
+kexec_reboot() {
+  current_kernel=$(uname -r)
+  new_kernel=$(rpm -qa kernel | tail -n 1)
+  new_kernel=${new_kernel#kernel-}
+  
+  if [[ $current_kernel != $new_kernel ]]; then 
+    kexec -l /boot/vmlinuz-${new_kernel} \
+      --initrd=/boot/initramfs-${new_kernel}.img \
+      --reuse-cmdline
+    sync
+    ( nohup sleep 2 && kexec -e & )
+    return
+  else
+    printf "No new kernel\n"
+  fi
+}
+
+if [[ -x /usr/sbin/kexec ]]; then
+  kexec_reboot
+else
+  echo reboot
+fi
+exit 0
